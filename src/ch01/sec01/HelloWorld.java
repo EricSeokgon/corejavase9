29dff8e80ac92580130fa3b0d5190ab9780d73c5
hadesyi <leesk55@gmail.com>
2018-04-13 23:57:01 +0900

    hello world

--- src/ch01/sec01/HelloWorld.java
+++ src/ch01/sec01/HelloWorld.java
@@ -1,7 +1,10 @@
 package ch01.sec01;
 
+import java.util.Random;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Hello, World!".length());
     }
 }
