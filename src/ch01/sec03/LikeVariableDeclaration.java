c212d4357585689e9dcede1046473515571820bd
hadesyi <leesk55@gmail.com>
2018-04-14 00:18:31 +0900

    변수 선언

---
+++ src/ch01/sec03/LikeVariableDeclaration.java
@@ -0,0 +1,11 @@
+package ch01.sec03;
+
+import java.util.Random;
+
+public class LikeVariableDeclaration {
+    public static void main(String[] args) {
+        int total = 0;
+        Random generator = new Random();
+
+    }
+}
