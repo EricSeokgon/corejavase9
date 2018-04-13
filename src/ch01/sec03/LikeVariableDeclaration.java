267e9b7bc6545018931ad54625491e3917d3042f
hadesyi <leesk55@gmail.com>
2018-04-14 00:41:51 +0900

    변수 초기화

--- src/ch01/sec03/LikeVariableDeclaration.java
+++ src/ch01/sec03/LikeVariableDeclaration.java
@@ -1,11 +1,15 @@
 package ch01.sec03;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class LikeVariableDeclaration {
     public static void main(String[] args) {
         int total = 0;
         Random generator = new Random();
 
+        Scanner in = new Scanner(System.in);
+        System.out.println("How old are you?");
+        int age = in.nextInt();
     }
 }
