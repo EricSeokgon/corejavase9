54bbe6ac99f7a243a3a626bc52938fd04f018afe
hadesyi <leesk55@gmail.com>
2018-04-14 00:47:48 +0900

    상수 선언

--- src/ch01/sec03/Calender.java
+++ src/ch01/sec03/Calender.java
@@ -2,4 +2,14 @@ package ch01.sec03;
 
 public class Calender {
     public static final int DAYS_PER_WEEK = 7;
+
+    public static void main(String[] args) {
+        final int DAYS_IN_FEBRUARY;
+        boolean leapYear = true;
+        if (leapYear) {
+            DAYS_IN_FEBRUARY = 29;
+        } else {
+            DAYS_IN_FEBRUARY = 28;
+        }
+    }
 }
