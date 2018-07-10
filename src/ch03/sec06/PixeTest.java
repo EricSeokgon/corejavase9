5e4933715934ab17643069be275e93f2036e8796
sklee <hadesleeq@gmail.com>
2018-07-10 13:57:04 +0900

    함수형 인터페이스

---
+++ src/ch03/sec06/PixeTest.java
@@ -0,0 +1,18 @@
+package ch03.sec06;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class PixeTest {
+
+    BufferedImage createimage(int width, int height, PixelFunction f) {
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                Color color = f.apply(x, y);
+                image.setRGB(x, y, color.getRGB());
+            }
+        }
+        return image;
+    }
+}
