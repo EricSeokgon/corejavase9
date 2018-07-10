d1f588390c516c541fb75ae59ac8f81e8cd74dd8
sklee <hadesleeq@gmail.com>
2018-07-10 14:06:32 +0900

    함수형 인터페이스

--- src/ch03/sec06/PixeTest.java
+++ src/ch03/sec06/PixeTest.java
@@ -5,7 +5,7 @@ import java.awt.image.BufferedImage;
 
 public class PixeTest {
 
-    BufferedImage createimage(int width, int height, PixelFunction f) {
+    static BufferedImage createImage(int width, int height, PixelFunction f) {
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
@@ -15,4 +15,10 @@ public class PixeTest {
         }
         return image;
     }
+
+    public static void main(String[] args) {
+        BufferedImage frenchFlag = createImage(150, 100, (x, y) -> x < 50 ? Color.BLUE : x < 100 ? Color.WHITE : Color.RED);
+
+    }
+
 }
