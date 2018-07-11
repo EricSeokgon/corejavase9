9be7b5036ee9ec99d58beb9de9ce131e6ea6b41a
sklee <hadesleeq@gmail.com>
2018-07-11 10:25:38 +0900

    추상 메서드와 추상 클래스

---
+++ src/ch04/sec01/Person.java
@@ -0,0 +1,15 @@
+package ch04.sec01;
+
+public abstract class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract int getId();
+}
