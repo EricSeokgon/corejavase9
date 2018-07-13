a5e55810d15fda7a0e51b7b6b813a2d05d07e3fe
sklee <hadesleeq@gmail.com>
2018-07-13 12:16:39 +0900

    예외 계층

---
+++ src/ch05/sec01/FileFormatException.java
@@ -0,0 +1,13 @@
+package ch05.sec01;
+
+import java.io.IOException;
+
+public class FileFormatException extends IOException {
+    public FileFormatException() {
+
+    }
+
+    public FileFormatException(String message) {
+        super(message);
+    }
+}
