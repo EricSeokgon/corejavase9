b7c6f01a2bbac578cb912d9263a119522ec72c77
sklee <hadesleeq@gmail.com>
2018-07-17 15:00:45 +0900

    단어 분리

--- src/ch08/sec01/sec1.java
+++ src/ch08/sec01/sec1.java
@@ -9,7 +9,26 @@ import java.util.List;
 public class sec1 {
     public static void main(String[] args) throws IOException {
         String contets = new String(Files.readAllBytes(
-                Paths.get("text.txt")), StandardCharsets.UTF_8);
+                Paths.get("C:\\windows-version.txt")), StandardCharsets.UTF_8);
         List<String> words = List.of(contets.split("\\PL+"));
+
+        int count = 0;
+        for (String w : words) {
+            if (w.length() > 12) {
+                count++;
+            }
+        }
+
+        long count2 = words.stream()
+                .filter(w -> w.length() > 12)
+                .count();
+
+        long count3 = words.parallelStream()
+                .filter(w -> w.length() > 12)
+                .count();
+
+        System.out.println(count);
+        System.out.println(count2);
+        System.out.println(count3);
     }
 }
