168121a67ba45a28d127269cc62e8636f9921e4a
sklee <hadesleeq@gmail.com>
2018-07-20 13:53:08 +0900

    filter, map, flatMap 메서드

--- src/ch08/sec03/sec03.java
+++ src/ch08/sec03/sec03.java
@@ -16,10 +16,14 @@ public class sec03 {
         Stream<String> longWords = words.stream().filter(w -> w.length() > 12);
         Stream<String> lowercaseWorlds = words.stream().map(String::toLowerCase);
         Stream<String> firstLetters = words.stream().map(s -> s.substring(0, 1));
+        Stream<Stream<String>> result = words.stream().map(w -> codePoints(w));
+        Stream<String> flatResult = words.stream().flatMap(w -> codePoints(w));
 
         System.out.println(longWords);
         System.out.println(lowercaseWorlds);
         System.out.println(firstLetters);
+        System.out.println(result);
+        System.out.println(flatResult);
     }
 
     public static Stream<String> codePoints(String s) {
