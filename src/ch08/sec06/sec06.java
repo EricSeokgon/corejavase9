a583fcd03935555ebab3b572fe36ef5ef06d144d
sklee <hadesleeq@gmail.com>
2018-07-24 10:27:55 +0900

    단순 리덕션

---
+++ src/ch08/sec06/sec06.java
@@ -0,0 +1,12 @@
+package ch08.sec06;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class sec06 {
+    public static void main(String[] args) {
+        Stream<String> words = Stream.of("홍길동", "유저인증", "자바");
+        Optional<String> largest = words.max(String::compareToIgnoreCase);
+        System.out.println("largest: " + largest.orElse(""));
+    }
+}
