7d421e2381a7cd1dc69bd89de45bc79f7febbf07
sklee <hadesleeq@gmail.com>
2018-07-26 09:39:00 +0900

    맵으로 모으기

---
+++ src/ch08/sec10/sec01.java
@@ -0,0 +1,18 @@
+package ch08.sec10;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class sec01 {
+    public static void main(String[] args) {
+        Stream<Locale> locales = Stream.of(Locale.getAvailableLocales());
+        Map<String, String> languageNames = locales.collect(Collectors.toMap(Locale::getDisplayLanguage, loc -> loc.getDisplayLanguage(loc),
+                (existingValue, newValue) -> existingValue));
+        Map<String, List<Locale>> countryToLocales = locales.collect(Collectors.groupingBy(Locale::getCountry));
+
+    }
+}
