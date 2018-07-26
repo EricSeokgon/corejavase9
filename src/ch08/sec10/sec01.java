6d315c1760631b42886de0d18c37710ec4e8d1b6
sklee <hadesleeq@gmail.com>
2018-07-26 09:51:44 +0900

    그루핑과 파티셔닝

--- src/ch08/sec10/sec01.java
+++ src/ch08/sec10/sec01.java
@@ -1,18 +1,21 @@
 package ch08.sec10;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class sec01 {
     public static void main(String[] args) {
         Stream<Locale> locales = Stream.of(Locale.getAvailableLocales());
-        Map<String, String> languageNames = locales.collect(Collectors.toMap(Locale::getDisplayLanguage, loc -> loc.getDisplayLanguage(loc),
+        Map<String, String> languageNames = locales.collect(
+                Collectors.toMap(
+                        Locale::getDisplayLanguage,
+                        loc -> loc.getDisplayLanguage(loc),
                 (existingValue, newValue) -> existingValue));
+
         Map<String, List<Locale>> countryToLocales = locales.collect(Collectors.groupingBy(Locale::getCountry));
+
+        List<Locale> swissLocales = countryToLocales.get("CH");
 
     }
 }
