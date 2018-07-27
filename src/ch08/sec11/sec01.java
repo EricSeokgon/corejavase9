692c24c7ded135617d2caac87a3d5612a162221f
sklee <hadesleeq@gmail.com>
2018-07-27 10:56:57 +0900

    다운스트림 컬렉터

---
+++ src/ch08/sec11/sec01.java
@@ -0,0 +1,16 @@
+package ch08.sec11;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toSet;
+
+public class sec01 {
+    public static void main(String[] args) {
+        Stream<Locale> locales = Stream.of(Locale.getAvailableLocales());
+        Map<String, Set<Locale>> countryToLocaleSet = locales.collect(groupingBy(Locale::getCountry, toSet()));
+    }
+}
