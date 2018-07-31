0b1d9b0923f270a64e15b8bdf76b7610714eb74b
sklee <hadesleeq@gmail.com>
2018-07-31 11:29:38 +0900

    기본 타입 스트림

--- src/ch08/sec13/sec01.java
+++ src/ch08/sec13/sec01.java
@@ -7,7 +7,13 @@ public class sec01 {
     public static void main(String[] args) {
         IntStream stream = IntStream.of(1, 1, 2, 3, 5);
         //stream = Arrays.stream(values, from, to);
-        IntStream zeroToNinetyNine = IntStream.range(0, 100);
+        IntStream zeroToNinetyNine = IntStream.range(0, 100); //상한 값을 제외한다.
+        IntStream zeroToHundered = IntStream.rangeClosed(0, 100); //상환 값을 포함한다.
+
+        String sentence = "\uD835\uDD46 is the set of octonions.";
+
+        IntStream cpdes = sentence.codePoints();
+
 
     }
 }
