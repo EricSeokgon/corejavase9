74677781de8c0e13b1b0b3c041f42cb47ae6a301
sklee <hadesleeq@gmail.com>
2018-08-20 08:50:49 +0900

    수신자를 명확하게 지정

--- src/ch12/sec01/sec01.java
+++ src/ch12/sec01/sec01.java
@@ -9,7 +9,16 @@ public class sec01 {
         runAlgorithm();
         Instant end = Instant.now();
         Duration timeElapsed = Duration.between(start, end);
+
+        Instant start2 = Instant.now();
+        runAlgorithm();
+        Instant end2 = Instant.now();
+        Duration timeElapsed2 = Duration.between(start2, end2);
         long millis = timeElapsed.toMillis();
+        Duration oneWeek = Duration.ofDays(7);
+        long secondsPerWeek = oneWeek.toSeconds();
+        boolean overTenTimesFaster = timeElapsed.multipliedBy(10).minus(timeElapsed2).isNegative();
+
 
     }
 
